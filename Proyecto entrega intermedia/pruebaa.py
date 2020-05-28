03bf3ba5352c33fc6dd2a62b9c39cba31cfa5cf4
jczl03 <66093859+jczl03@users.noreply.github.com>
2020-05-28 17:59:45 -0500

    Carpeta completa

---
+++ Proyecto entrega intermedia/pruebaa.py
@@ -0,0 +1,18 @@
+#Datos de cada cita: 
+#Referencia (Espec. del médico)
+#Fecha
+#Hora
+#Booleano (si está disponible o no)
+#Nombre del medico
+#Nombre del paciente
+citas_usuarios={}
+citas_medicos={"1004554648":[{"fecha:":blablabla}{"fdcha2":blabla2}]}
+#Función para que el medico haga disponible una cita:
+def x():
+    c={}
+    c["fecha"]=#v fecha
+    #...
+    citas disponibles :v
+for x in citas_medicos[#cc medico]:
+
+    
